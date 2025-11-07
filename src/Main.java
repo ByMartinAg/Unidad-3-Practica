56971c5825d626a4a050f26d4c94386939a87ce6
Martin <martingalindo3202@gmail.com>
2025-11-06 23:37:59 -0600

    Nueva Estructura de clases

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-public class Main {
+import java.util.Scanner;
+ class Main {
     public static void main(String[] args) {
-        MenuPlatillos menu = new MenuPlatillos();
-        MenuPlatillos administrar = new MenuPlatillos();
-        Pedidos
 
 
-        // menu.MostrarMenu();
     }
 }
